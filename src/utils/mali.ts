4c023f8f68ac5570189dd018f9e5265a78a8ab82
jvstln <173433315+jvstln@users.noreply.github.com>
2025-04-27 21:44:02 +0100

    fix: update mail.ts config and .env.example

--- src/utils/mali.ts
+++ src/utils/mali.ts
@@ -4,21 +4,25 @@ import nodemailer from "nodemailer";
 // Create a transporter object
 const transporter = nodemailer.createTransport({
   host: process.env.NODEMAILER_HOST,
-  port: 587,
-  secure: false, // use SSL
+  // port: 587,
+  port: 465,
+  secure: true, // use SSL
   auth: {
     user: process.env.NODEMAILER_USER,
     pass: process.env.NODEMAILER_PASSWORD,
   },
 });
 
-// Send the email function
-export const sendEmail = (mailOptions: {
+export const sendEmail = async (mailOptions: {
   from?: string;
   to: string;
   subject: string;
   text?: string;
   html?: string;
 }) => {
-  return transporter.sendMail({ from: "Gbese Team", ...mailOptions });
+  try {
+    return await transporter.sendMail({ from: "Gbese Team", ...mailOptions });
+  } catch (error) {
+    console.log("Error sending email:", error);
+  }
 };
